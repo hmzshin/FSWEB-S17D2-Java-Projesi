f4392f7ca23153826e146b41bf86bbecd7ace1f1
hmzshin <hamzasahin@outlook.com>
2024-02-06 23:03:12 +0300

    create DeveloperTax class

---
+++ src/main/java/com/workintech/s17d2/tax/DeveloperTax.java
@@ -0,0 +1,22 @@
+package com.workintech.s17d2.tax;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class DeveloperTax implements Taxable {
+    @Override
+    public double getSimpleTaxRate() {
+
+        return 0.25;
+    }
+
+    @Override
+    public double getMiddleTaxRate() {
+        return 0.20;
+    }
+
+    @Override
+    public double getUpperTaxRate() {
+        return 0.30;
+    }
+}
