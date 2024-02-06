cab58992230f0ced250a11d7fab1c5f7936bcd65
hmzshin <hamzasahin@outlook.com>
2024-02-06 11:23:44 +0300

    create sprint boot aplication

---
+++ src/main/java/com/workintech/s17d2/DependecyInjection/DependecyInjectionApplication.java
@@ -0,0 +1,13 @@
+package com.workintech.s17d2.DependecyInjection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DependecyInjectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DependecyInjectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/workintech/s17d2/DependecyInjection/DependecyInjectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workintech.s17d2.DependecyInjection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DependecyInjectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
