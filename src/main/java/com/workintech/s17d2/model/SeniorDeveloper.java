2e89d7cc9797fd7cb33cc2533b93aa076c6647cd
hmzshin <hamzasahin@outlook.com>
2024-02-06 23:04:17 +0300

    create SeniorDeveloper class

---
+++ src/main/java/com/workintech/s17d2/model/SeniorDeveloper.java
@@ -0,0 +1,9 @@
+package com.workintech.s17d2.model;
+
+import com.workintech.s17d2.rest.Experience;
+
+public class SeniorDeveloper extends Developer {
+    public SeniorDeveloper(int id, String name, double salary, Experience experience) {
+        super(id, name, salary, experience);
+    }
+}
