b869772e96d804bf6243310c8b722e5930aa19a8
hmzshin <hamzasahin@outlook.com>
2024-02-06 23:04:06 +0300

    create MidDeveloper class

---
+++ src/main/java/com/workintech/s17d2/model/MidDeveloper.java
@@ -0,0 +1,9 @@
+package com.workintech.s17d2.model;
+
+import com.workintech.s17d2.rest.Experience;
+
+public class MidDeveloper extends Developer {
+    public MidDeveloper(int id, String name, double salary, Experience experience) {
+        super(id, name, salary, experience);
+    }
+}
